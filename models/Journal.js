5f759ca19decf8c225c14fa211bbe76c5f157fd8
Kiri <kirilynne@gmail.com>
2021-11-02 09:15:42 -0400

    add model to Journal.js

---
+++ models/Journal.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Journal extends Model { }
+
+Journal.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        destination_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'location',
+                key: 'id',
+            },
+        },
+        way_point_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'location',
+                key: 'id',
+            },
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'journal',
+    }
+);
+
+module.exports = Journal;
