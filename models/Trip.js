66100537defc8d54dd3622958e3a648e84a07ae7
Kiri <kirilynne@gmail.com>
2021-11-02 04:45:07 -0400

    add Trip model

---
+++ models/Trip.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Trip extends Model { }
+
+Trip.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        destination_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'locations',
+                key: 'id',
+            },
+        },
+        start_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'locations',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'trip',
+    }
+);
+
+module.exports = Trip;
