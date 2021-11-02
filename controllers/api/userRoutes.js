99217dc5b42b2ec45a5122fbfec161404ef9f77d
Kiri <kirilynne@gmail.com>
2021-11-02 16:19:41 -0400

    update userRoutes.js

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,54 @@
+const router = require('express').Router();
+const { User, Trip, Location, Journal, Packlist } = require('../models');
+const withAuth = require('../../utils/auth');
+
+// Route to create an account and register a new user
+router.post('/', async (req, res) => {
+    try {
+      const userData = await User.create(req.body);
+
+      // set up a session to store the user information 
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.name = userData.name;
+        req.session.logged_in = true;
+  
+        res.status(200).json(userData);
+      });
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+  // route for user sign in
+  router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: { email: req.body.email } });
+
+        if (!userData) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect email or password. Try again.' });
+            return;
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect email or password. Try again.' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.json({ user: userData, message: 'You have been logged in successfully!' });
+        });
+
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
