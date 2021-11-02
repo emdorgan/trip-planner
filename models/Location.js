480251b0ee0d4f0a09aaead88f8a25a06f96534f
Kiri <kirilynne@gmail.com>
2021-11-02 13:21:09 -0400

    update Location model

--- models/Location.js
+++ models/Location.js
@@ -3,85 +3,87 @@ const sequelize = require("../config/connection");
 
 class Location extends Model { }
 
-
-//Sequence
-
 Location.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        location_name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        location_address: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            /*
-                        name: Sequelize.STRING,
-                        address: Sequelize.STRING,
-                        latitude: {
-                          type: DataTypes.INTEGER,
-                          validate: {
-                            min: -90,
-                            max: 90
-                          }
-                        },
-                        longitude: {
-                          type: DataTypes.INTEGER,
-                          validate: {
-                            min: -180,
-                            max: 180
-                          }
-                        },
-                      }, {
-                        sequelize,
-                        validate: {
-                          bothCoordsOrNone() {
-                            if ((this.latitude === null) !== (this.longitude === null)) {
-                              throw new Error('Either both latitude and longitude, or neither!');
-                            }
-                          }
-                        }
-                      })*/
-        },
-        vehicle: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        start_date: {
-            type: DataTypes.DATE,
-            allowNull: false,
-            //add
-        },
-        duration: {
-            type: DataTypes.DATE,
-            allowNull: false,
-            //add
-        },
-        location_activities: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        trip_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'trip',
-                key: 'id',
-            },
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'location',
+    location_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    location_address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      /*
+                  name: Sequelize.STRING,
+                  address: Sequelize.STRING,
+                  latitude: {
+                    type: DataTypes.INTEGER,
+                    validate: {
+                      min: -90,
+                      max: 90
+                    }
+                  },
+                  longitude: {
+                    type: DataTypes.INTEGER,
+                    validate: {
+                      min: -180,
+                      max: 180
+                    }
+                  },
+                }, {
+                  sequelize,
+                  validate: {
+                    bothCoordsOrNone() {
+                      if ((this.latitude === null) !== (this.longitude === null)) {
+                        throw new Error('Either both latitude and longitude, or neither!');
+                      }
+                    }
+                  }
+                })*/
+    },
+    vehicle: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    start_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      //add
+    },
+    end_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      //add
+    },
+    location_activities: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    trip_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'trip',
+        key: 'id',
+      },
+    },
+    sequence: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true,
     }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'location',
+  }
 );
 
 module.exports = Location;
