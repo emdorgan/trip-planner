dbd55a51ed39642cc6d70258f211c151e47f5f25
Daniel Vo <daniel.vo@berkeley.edu>
2021-11-04 13:27:30 -0700

    created post route to create new image instance

--- controllers/api/imageRoutes.js
+++ controllers/api/imageRoutes.js
@@ -4,9 +4,16 @@ const upload = require('../../utils/upload');
 const { Image } = require('../../models');
 
 // route to post 
-router.post('/upload', withAuth, upload.single('image'), (req, res) => {
-    console.log(req.body);
-    res.render("album");
+router.post('/', withAuth, upload.single('image'), async (req, res) => {
+    try {
+        const newImage = await Image.create({
+            user_id: req.session.user_id,
+            filename: req.file.filename,
+        });
+        res.status(200).json(newImage);
+    } catch (err) {
+        res.status(400).json(err);
+    }
 });
 
 module.exports = router;
